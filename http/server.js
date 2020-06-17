80a43004fdad95d3bd5c91be7d7bcdb3900071b9
Jefferson da Silva <je_jss@hotmail.com>
2020-06-17 00:37:33 -0300

    update folder

---
+++ http/server.js
@@ -0,0 +1,25 @@
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+http.createServer((req, res)  => {
+    const file = req.url === '/' ? 'index.html' : req.url
+    const filePath = path.join(__dirname, 'public', 'index.html')
+    const extname = path.extname(filePath)
+
+    const allowedFiles = ['.html', '.css', '.js']
+    const allowed = allowedFiles.find( item => item == extname)
+
+
+    if (!allowed) return 
+    if(req.url === '/') {
+        fs.readFile(filePath,
+        (err, content) => {
+            if (err) throw err;
+
+            res.end(content)
+        }
+        
+        )
+    }
+}).listen(3333, () => console.log('Server is Running'));
